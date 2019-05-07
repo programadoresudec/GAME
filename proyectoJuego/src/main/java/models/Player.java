82631c5f04657a416f7bf6bfc45944ddf2ac4560
Diego Arturo Parra Molina <48141200+DiegoArturoParra@users.noreply.github.com>
2019-05-06 21:44:47 -0500

    model Player
    creacion de atributos para los datos del jugador

---
+++ proyectoJuego/src/main/java/models/Player.java
@@ -0,0 +1,53 @@
+package models;
+
+/**
+ */
+public class Player
+{
+   private String name;
+   private String country;
+   private int score;
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the country
+     */
+    public String getCountry() {
+        return country;
+    }
+
+    /**
+     * @param country the country to set
+     */
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    /**
+     * @return the score
+     */
+    public int getScore() {
+        return score;
+    }
+
+    /**
+     * @param score the score to set
+     */
+    public void setScore(int score) {
+        this.score = score;
+    }
+   
+}

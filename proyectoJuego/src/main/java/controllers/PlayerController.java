94a379e063475f8403ca8b257ba1a2fb9707b330
Diego Arturo Parra Molina <diegop177@hotmail.com>
2019-05-17 14:43:15 -0500

    controlador con el modelo player
    programacion del boton para que ingrese jugador a un arraylist

--- proyectoJuego/src/main/java/controllers/PlayerController.java
+++ proyectoJuego/src/main/java/controllers/PlayerController.java
@@ -1,14 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package controllers;
+// Importanmos con * para llamar todas las librerias dependiendo si es un swing o awt o awt.event.
+import java.awt.event.*;
+import javax.swing.*;
+//Se importan las librerías de java.util.* donde se concentran la mayor parte de las Clases del "Collection Framework". 
+import java.util.*;
 
 /**
  *
  * @author LaurayDiego
  */
-public class PlayerController {
+public class PlayerController implements ActionListener{
+
+    @Override
+    public void actionPerformed(ActionEvent e) 
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
